dbdaf0d8fa265a5a2e2ddf4cbc1302765d0cb6c4
Dario Lencina <dario@securityunion.dev>
2024-05-01 21:22:15 -0400

    checkin initial code

---
+++ control-accelerometer/target/debug/deps/control_accelerometer-26c8d52fb8ff6111.d
@@ -0,0 +1,5 @@
+/home/ubuntu/gpio-rust/control-accelerometer/target/debug/deps/libcontrol_accelerometer-26c8d52fb8ff6111.rmeta: src/main.rs
+
+/home/ubuntu/gpio-rust/control-accelerometer/target/debug/deps/control_accelerometer-26c8d52fb8ff6111.d: src/main.rs
+
+src/main.rs:
---
+++ control-accelerometer/target/debug/deps/control_accelerometer-b8ff8d2cff2161a1.d
@@ -0,0 +1,5 @@
+/home/ubuntu/gpio-rust/control-accelerometer/target/debug/deps/libcontrol_accelerometer-b8ff8d2cff2161a1.rmeta: src/main.rs
+
+/home/ubuntu/gpio-rust/control-accelerometer/target/debug/deps/control_accelerometer-b8ff8d2cff2161a1.d: src/main.rs
+
+src/main.rs:
