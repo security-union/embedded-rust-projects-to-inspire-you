2825617b8249e2132f87f422588a82db4d44cb87
Dario Lencina <dario@securityunion.dev>
2024-05-10 00:40:41 -0400

    save

--- control-accelerometer/src/constants/mod.rs
+++
@@ -1,9 +0,0 @@
-use std::net::Ipv4Addr;
-
-use lazy_static::lazy_static;
-
-lazy_static! {
-    pub static ref BROADCAST_IP: Ipv4Addr = Ipv4Addr::new(224, 0, 0, 123);
-    pub static ref BROADCAST_PORT: u16 = 7645;
-    pub static ref ANY: Ipv4Addr = "0.0.0.0".parse().unwrap();
-}
