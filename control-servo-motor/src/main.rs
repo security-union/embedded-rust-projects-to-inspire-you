3f7cda65b732bb55d2e932e98e72dbd64158fe85
Dario Lencina <dario@securityunion.dev>
2024-06-09 23:13:00 -0400

    simplify servo controlle4r

--- control-servo-motor/src/main.rs
+++ control-servo-motor/src/main.rs
@@ -2,15 +2,13 @@ use gpio_cdev::{Chip, LineRequestFlags};
 use std::time::Instant;
 
 const GPIO13: u32 = 6;
-const PWM_FREQUENCY_HZ: f64 = 50.0; // 50 Hz corresponds to a 20 ms period
-const PERIOD_MS: u64 = 20; // 20 ms period
-const PULSE_MIN_US: u64 = 1200; // Minimum pulse width
-const PULSE_NEUTRAL_US: u64 = 1500; // Neutral pulse width
-const PULSE_MAX_US: u64 = 1800; // Maximum pulse width
-const CYCLE_DURATION_SECONDS: u64 = 20;
+const PERIOD_US: f64 = 20.0; // 20 ms period
+const PULSE_MIN_US: f64 = 1200.0; // Minimum pulse width
+const PULSE_MAX_US: f64 = 1800.0; // Maximum pulse width
+const CYCLE_DURATION_SECS: f64 = 10.0;
 
 /**
- * This program will smoothly change the pulse width of a servo from PULSE_MIN_US to PULSE_MAX_US over 20 seconds.
+ * This program will smoothly change the pulse width of a servo from PULSE_MIN_US to PULSE_MAX_US over CYCLE_DURATION_SECS seconds.
  */
 fn main() -> Result<(), gpio_cdev::Error> {
     // Open the GPIO chip; it's gpiochip0 for the main GPIO controller on a Raspberry Pi
@@ -18,20 +16,18 @@ fn main() -> Result<(), gpio_cdev::Error> {
     let line = chip.get_line(GPIO13)?;
     let line = line.request(LineRequestFlags::OUTPUT, 0, "pwm")?;
     loop {
-        let start_time = Instant::now();
-
+        let start_time: Instant = Instant::now();
         loop {
             let elapsed_time = start_time.elapsed().as_secs_f64();
-            if elapsed_time >= CYCLE_DURATION_SECONDS as f64 {
+            if elapsed_time >= CYCLE_DURATION_SECS {
                 break;
             }
-
             // Calculate the pulse width based on the elapsed time
-            let pulse_width_us = PULSE_MIN_US as f64
-                + (elapsed_time / CYCLE_DURATION_SECONDS as f64)
-                    * (PULSE_MAX_US as f64 - PULSE_MIN_US as f64);
+            let pulse_width_us: f64 = PULSE_MIN_US
+                + (elapsed_time / CYCLE_DURATION_SECS)
+                    * (PULSE_MAX_US - PULSE_MIN_US);
             let time_on_us = pulse_width_us;
-            let time_off_us = (PERIOD_MS * 1000) as f64 - time_on_us;
+            let time_off_us = PERIOD_US - time_on_us;
 
             println!(
             "Elapsed time: {:.2} s, Pulse width: {:.2} µs, Time on: {:.2} µs, Time off: {:.2} µs",
@@ -44,6 +40,4 @@ fn main() -> Result<(), gpio_cdev::Error> {
             std::thread::sleep(std::time::Duration::from_micros(time_off_us as u64));
         }
     }
-
-    Ok(())
 }
