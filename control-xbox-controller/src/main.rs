6733b0bfd54b396f929d3e3886f70a7f75e68ef8
Dario Lencina <dario@securityunion.dev>
2024-06-09 22:36:55 -0400

    switched controller code to be flatter

--- control-xbox-controller/src/main.rs
+++ control-xbox-controller/src/main.rs
@@ -1,27 +1,25 @@
-use gilrs::{Button, Event, Gilrs};
+use gilrs::{Axis, Button, Event, Gilrs};
+use anyhow::Result;
 
-fn main() {
+fn main() -> Result<()> {
     let mut gilrs = Gilrs::new().unwrap();
-
-    // Iterate over all connected gamepads
     for (_id, gamepad) in gilrs.gamepads() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
-    
-    let mut active_gamepad = None;
-    
     loop {
-        // Examine new events
         while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            active_gamepad = Some(id);
-        }
-    
-        // You can also use cached gamepad state
-        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
+            match event {
+                gilrs::EventType::AxisChanged(Axis::LeftStickY, value,code) => {
+                    println!("Left Stick Y: {}", value);   
+                },
+                gilrs::EventType::AxisChanged(Axis::RightStickX, value, code) => {
+                    println!("Right Stick X: {}", value);
+                },
+                _ => {
+
+                }
             }
         }
     }
+    Ok(())
 }
