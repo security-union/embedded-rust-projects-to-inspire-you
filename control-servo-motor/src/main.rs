c1679bb49f662c044f66749e2c285efe06b42255
Dario Lencina <dario@securityunion.dev>
2024-06-11 22:27:06 -0400

    taylor sucks

--- control-servo-motor/src/main.rs
+++ control-servo-motor/src/main.rs
@@ -2,7 +2,7 @@ use gpio_cdev::{Chip, LineRequestFlags};
 use std::time::Instant;
 
 const GPIO13: u32 = 6;
-const PERIOD_US: f64 = 20.0; // 20 ms period
+const PERIOD_US: f64 = 20000f64; // 20 ms period
 const PULSE_MIN_US: f64 = 1200.0; // Minimum pulse width
 const PULSE_MAX_US: f64 = 1800.0; // Maximum pulse width
 const CYCLE_DURATION_SECS: f64 = 10.0;
@@ -23,16 +23,15 @@ fn main() -> Result<(), gpio_cdev::Error> {
                 break;
             }
             // Calculate the pulse width based on the elapsed time
-            let pulse_width_us: f64 = PULSE_MIN_US
-                + (elapsed_time / CYCLE_DURATION_SECS)
-                    * (PULSE_MAX_US - PULSE_MIN_US);
-            let time_on_us = pulse_width_us;
+            let pulse_delta = PULSE_MAX_US - PULSE_MIN_US;
+            let time_on_us_increment = (elapsed_time / CYCLE_DURATION_SECS) * pulse_delta;
+            let time_on_us: f64 = PULSE_MIN_US + time_on_us_increment;
             let time_off_us = PERIOD_US - time_on_us;
 
             println!(
-            "Elapsed time: {:.2} s, Pulse width: {:.2} µs, Time on: {:.2} µs, Time off: {:.2} µs",
-            elapsed_time, pulse_width_us, time_on_us, time_off_us
-        );
+                "Elapsed time: {:.2} s, Time on: {:.2} µs, Time off: {:.2} µs",
+                elapsed_time, time_on_us, time_off_us
+           );
 
             line.set_value(1)?;
             std::thread::sleep(std::time::Duration::from_micros(time_on_us as u64));
